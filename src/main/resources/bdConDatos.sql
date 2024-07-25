1a9553a45413ff3b03e880e3dd9b75dd4c8a3c4e
juanjbetancur <juan.jbetancur852@gmail.com>
2024-07-25 13:08:02 -0500

    carga base con datos

---
+++ src/main/resources/bdConDatos.sql
@@ -0,0 +1,11 @@
+USE depgirpro;
+USE burguerdb;
+
+SELECT * FROM personas;
+SELECT * FROM equipos;
+SELECT * FROM encuentros;
+SELECT * FROM torneos;
+UPDATE personas SET rol_id = 1 WHERE correo = "juanj@gmail.com"
+
+
+$2a$12$kKAfi38t3Ta76cuxrdgwHeSB4HmBT44MeTB2C5.k65tGtKnYzs9OO
